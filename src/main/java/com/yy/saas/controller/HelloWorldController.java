266842c7f7c00326affd113f5184d4276fdf747e
luke wang <wxl_954136@163.com>
2020-03-22 16:42:29 +0800

    test 2020.3.20

--- src/main/java/com/yy/saas/controller/HelloWorldController.java
+++ src/main/java/com/yy/saas/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.yy.saas.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -8,6 +9,7 @@ public class HelloWorldController {
     @RequestMapping("/say")
     public String say()
     {
+        System.out.println("1111");
         return "hello 王晓陆!你知道正确吗";
     }
 }
